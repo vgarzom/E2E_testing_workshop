63f06489aa0778075d325ebb962a6f7810a196ac
victor <vgarzom@gmail.com>
2019-08-24 19:22:54 -0500

    first step: cypress login success

---
+++ cypress_test/cypress/integration/simple_spec.js
@@ -0,0 +1,13 @@
+describe('Los estudiantes login', function() {
+  it('Visits los estudiantes and fails at login', function() {
+      cy.visit('https://losestudiantes.co')
+      cy.contains('Cerrar').click()
+      //
+      cy.contains('Ingresar').click()
+      cy.get('.cajaLogIn').find('input[name="correo"]').click().type("charlietester@mailinator.com")
+      cy.get('.cajaLogIn').find('input[name="password"]').click().type("test1234")
+      cy.get('.cajaLogIn').contains('Ingresar').click()
+      //cy.contains('El correo y la contraseña que ingresaste no figuran en la base de datos. Intenta de nuevo por favor.')
+      cy.get('#cuenta')
+  })
+})
