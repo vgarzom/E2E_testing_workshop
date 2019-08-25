6777474a38200577da7e3785fa0e75d4381264ad
victor <vgarzom@gmail.com>
2019-08-24 21:27:21 -0500

    search a teacher and open his page

---
+++ cypress_test/cypress/integration/teacher_search.js
@@ -0,0 +1,12 @@
+describe('Los estudiantes login', function() {
+  it('Visits los estudiantes and fails at login', function() {
+      cy.visit('https://losestudiantes.co')
+      cy.contains('Cerrar').click()
+      //
+      //Buscamos el input de profesores
+      //cy.get('.Select-input').find('input').type("Mario Linares", {force: true})
+      cy.get('.Select-input').click().type("Mario Linares")
+      //La busqueda anterior debe devolver un único resultado. Nos dirigimos a la página del profesor
+      //cy.get('.Select-option').click()
+  })
+})
